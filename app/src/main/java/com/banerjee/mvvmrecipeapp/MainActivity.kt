f8b6181637db4addc7d7d9577b6f03d6d56abccf
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2021-04-18 19:00:08 +0530

    Column and Row tested

--- app/src/main/java/com/banerjee/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/banerjee/mvvmrecipeapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.banerjee.mvvmrecipeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,14 +25,79 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             Column(
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color(0xFFF2F2F2)),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = "Hey look some text")
-                Spacer(modifier = Modifier.padding(10.dp))
-                Button(onClick = {}) {
-                    Text(text = "A BUTTON")
+                Image(
+                    painter = painterResource(R.drawable.happy_meal_small),
+                    contentDescription = "Happy Meal Image",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(modifier = Modifier.padding(16.dp)) {
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Happy Meal",
+                            style = TextStyle(
+                                fontSize = 26.sp
+                            )
+                        )
+
+                        Text(
+                            text = "$5.99",
+                            style = TextStyle(
+                                color = Color(0xFF85bb65),
+                                fontSize = 17.sp
+                            ),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
+
+
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(
+                        text = "800 Calories",
+                        style = TextStyle(
+                            fontSize = 17.sp
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "BUY NOW")
+                    }
                 }
             }
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
