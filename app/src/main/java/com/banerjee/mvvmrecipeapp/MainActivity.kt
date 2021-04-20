c1c1f302d5d39696ce650e763ed5487a5438542e
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2021-04-20 22:48:56 +0530

    Domain Recipe model added

--- app/src/main/java/com/banerjee/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/banerjee/mvvmrecipeapp/MainActivity.kt
@@ -22,65 +22,11 @@ import androidx.compose.ui.unit.sp
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-                    .background(color = Color(0xFFF2F2F2)),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.happy_meal_small),
-                    contentDescription = "Happy Meal Image",
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
-                )
-
-                Column(modifier = Modifier.padding(16.dp)) {
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Happy Meal",
-                            style = TextStyle(
-                                fontSize = 26.sp
-                            )
-                        )
-
-                        Text(
-                            text = "$5.99",
-                            style = TextStyle(
-                                color = Color(0xFF85bb65),
-                                fontSize = 17.sp
-                            ),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-
-
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                        text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = 17.sp
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-
-                    Button(
-                        onClick = {},
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "BUY NOW")
-                    }
-                }
-            }
-
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, RecipeListFragment())
+            .commit()
     }
 }
 
